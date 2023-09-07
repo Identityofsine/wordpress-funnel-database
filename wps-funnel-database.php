caac36eabf1f4e4720ab784b795ba396a63cd99d
Identityofsine <67929513+Identityofsine@users.noreply.github.com>
2023-09-06 20:57:16 -0400

    changed version

--- wps-funnel-database.php
+++ wps-funnel-database.php
@@ -2,7 +2,7 @@
 /*
 * Plugin Name:       RESTful Funnel Database
 * Description:       Creates a REST API for the funnel database, allows for POST requests to submit data into a database
-* Version:           1.2
+* Version:           2.0 
 * Requires at least: 5.2
 * Requires PHP:      7.2
 * Author:            Kevin Erdogan
