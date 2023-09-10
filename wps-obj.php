cf05b4b5c49b81a29b290a06c997e20dae98d218
Identityofsine <67929513+Identityofsine@users.noreply.github.com>
2023-09-10 18:37:49 -0400

    added new funnel_object

---
+++ wps-obj.php
@@ -0,0 +1,43 @@
+<?php
+
+/*
+ * 	$sql = "CREATE TABLE IF NOT EXISTS $table_name (
+ *	id mediumint(9) NOT NULL AUTO_INCREMENT,
+		funnel_message varchar(255) NOT NULL,
+		active boolean NOT NULL DEFAULT FALSE,
+		phone boolean NOT NULL DEFAULT TRUE,
+		hero_image varchar(255),
+		header_icon varchar(255), 
+		header_text varchar(255),
+		header_subtext varchar(255),
+		button_text varchar(255),
+		PRIMARY KEY (id)
+	) $charset_collate;";
+ */
+
+class FunnelObject {
+	public $id;
+	public $message;
+	public $active;
+	public $phone;
+	public $hero_image;
+	public $header_icon;
+	public $header_text;
+	public $header_subtext;
+	public $button_text;
+
+	//-1 id = new object
+	public function __construct($id = -1, $message = '', $active = true, $phone = true, $hero_image = '', $header_icon = '', $header_text = '', $header_subtext = '', $button_text = '') {
+		$this->id = $id;
+		$this->message = $message;
+		$this->active = $active;
+		$this->phone = $phone;
+		$this->hero_image = $hero_image;
+		$this->header_icon = $header_icon;
+		$this->header_text = $header_text;
+		$this->header_subtext = $header_subtext;
+		$this->button_text = $button_text;
+	}
+
+	
+} 
